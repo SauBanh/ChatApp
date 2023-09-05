af32db892ec1b951c0c8d97015325f5cdd2ceb7d
SauBanh <saubanh05062001@gmail.com>
2023-09-05 18:31:31 +0700

    update config

--- API/app.ts
+++ API/app.ts
@@ -1,2 +1,12 @@
-const greeting: string = "Xin chào, Tuấn Anh đẹp trai!";
-console.log(greeting);
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
